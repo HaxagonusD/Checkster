670ce2f46cc977063ff8903a6d6b074794eb607f
Shafee <shafeelinks@gmail.com>
2021-01-30 16:40:10 -0500

    fixed margin issue around steps.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Nav from "./components/Nav";
 import About from "./components/About";
+import Results from "./components/Results";
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 				<Switch>
 					<Route exact path="/" component={Home} />
 					<Route exact path="/about" component={About} />
+					<Route exact path="/results" component={Results} />
 				</Switch>
 			</div>
 		</Router>
---
+++ client/src/components/Results.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Results() {
+	return (
+		<div>
+			<h1>Results Page</h1>
+		</div>
+	);
+}
