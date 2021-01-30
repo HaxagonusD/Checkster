7ceb1dddf2c193433a927d10174653e388d9d02a
Ayman Omer <aymangebrilomer@gmail.com>
2021-01-30 01:57:14 -0500

    first commit

--- server/index.js
+++ server/index.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send(" hey im here ");
+});
+app.get("/", function (req, res, next) {
+  console.log(res);
+});
+
 app.listen(5000, () => {
   console.log("Listening on post 5000");
 });
