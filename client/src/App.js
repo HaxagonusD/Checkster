36ac8d31c0d43703f9b0985022b7eeee682b45bb
Shafee <shafeelinks@gmail.com>
2021-01-30 00:31:41 -0500

    tailwind added

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import Nav from "./components/Nav";
+import About from "./components/About";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
 import Steps from "./components/Steps";
@@ -7,6 +9,7 @@ import Upload from "./components/Upload";
 function App() {
 	return (
 		<div className="App">
+			<Nav />
 			<Header />
 			<Banner />
 			<Steps />
--- client/src/components/Upload.js
+++ client/src/components/Upload.js
@@ -1,9 +1,26 @@
 import React from "react";
 
-export default function Upload() {
+function FileUpload() {
+	const [file, setFile] = React.useState("");
+	function handleUpload(event) {
+		setFile(event.target.files[0]);
+	}
+
 	return (
-		<div>
-			<h1>Hello from Upload</h1>
+		<div id="upload-box">
+			<input type="file" onChange={handleUpload} />
+			<p>Filename: {file.name}</p>
+			<p>File type: {file.type}</p>
+			<p>File size: {file.size} bytes</p>
+			{file && <ImageThumb image={file} />}
 		</div>
 	);
+}
+
+const ImageThumb = ({ image }) => {
+	return <img src={URL.createObjectURL(image)} alt={image.name} />;
+};
+
+export default function App() {
+	return <FileUpload />;
 }
