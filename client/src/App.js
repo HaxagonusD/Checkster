6407dfbada0995ff311bddafe58fac9d50dc5c03
Shafee <shafeelinks@gmail.com>
2021-01-29 23:04:26 -0500

    folder structure is set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import Steps from "./components/Steps";
+import Upload from "./components/Upload";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<Banner />
+			<Steps />
+			<Upload />
+		</div>
+	);
 }
 
 export default App;
---
+++ client/src/components/Banner.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Banner() {
+	return (
+		<div>
+			<h1>Hello from Banner</h1>
+		</div>
+	);
+}
