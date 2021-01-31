97a08066b89b8a17c29164cf66a79bf1eeeef5d8
Leesel Fraser <leeselfraser08@gmail.com>
2021-01-30 19:28:53 -0500

    leesel

---
+++ client/src/components/pages/SignUp.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "../../App.css";
+
+export default function SignUp() {
+  return <h1 className="sign-up">INSERT VIDEO OF THEM CHECKS BEING READ</h1>;
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-ReactDOM.render(
-	<React.StrictMode>
-		<Router>
-			<App />
-		</Router>
-	</React.StrictMode>,
-	document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById('root'));
