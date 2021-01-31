26c4c0e1800be18350a172c60e54ab7784495d16
Shafee <shafeelinks@gmail.com>
2021-01-31 00:30:43 -0500

    styled the results page a bit

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,15 +1,3 @@
-// import React from 'react';
-// import './Button.css';
-// import { Link } from 'react-router-dom';
-
-// export function Button() {
-//   return (
-//     <Link to='sign-up'>
-//       <button className='btn'>Sign Up</button>
-//     </Link>
-//   );
-// }
-
 import React from "react";
 import "./Button.css";
 import { Link } from "react-router-dom";
