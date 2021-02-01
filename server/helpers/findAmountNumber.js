acb34d9ae3ca07e1d0274a5ebf1305f952dcd570
HaxagonusD <jquezad1@binghamton.edu>
2021-02-01 01:20:51 -0500

    Everything is working now

--- server/helpers/findAmountNumber.js
+++ server/helpers/findAmountNumber.js
@@ -1,5 +1,5 @@
 //function that finds the amount number of the check
 module.exports = function findAmountNumber(text) {
-  const matches = text.match(/\$?\s*?[0-9\,]+?[.][0-9][0-9]/);
+  const matches = text.match(/\$\s*?[0-9\,]+([\.][0-9][0-9])?/);
   return matches ? matches[0] : matches;
 };
